72dea7c24ec0bc46410854086a27db53b18bf5ad
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2020-07-24 16:36:11 +0300

    Linux: create very basic X11 window

--- src/linux_main.c
+++ src/linux_main.c
@@ -1,1 +1,35 @@
-int main(int argc, char *argv) { return 0; }
+#include "tk_platform.h"
+#include <X11/Xlib.h>
+
+int main(int argc, char *argv[]) {
+  Display *display;
+  Window window;
+  Screen *screen;
+  s32 screen_id;
+  XEvent ev;
+
+  display = XOpenDisplay(NULL);
+  if (!display) {
+    return 1;
+  }
+
+  screen = DefaultScreenOfDisplay(display);
+  screen_id = DefaultScreen(display);
+
+  window = XCreateSimpleWindow(display, RootWindowOfScreen(screen), 0, 0, 1280,
+                               720, 1, BlackPixel(display, screen_id),
+                               WhitePixel(display, screen_id));
+
+  XClearWindow(display, window);
+  XMapRaised(display, window);
+
+  while (true) {
+    XNextEvent(display, &ev);
+  }
+
+  XDestroyWindow(display, window);
+  XFree(screen);
+  XCloseDisplay(display);
+
+  return 0;
+}
