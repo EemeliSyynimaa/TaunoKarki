2b138872fcb3f732d8791405c7adf006719a22a7
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2020-09-28 11:08:17 +0300

    Linux: add todos

--- src/linux_main.c
+++ src/linux_main.c
@@ -7,9 +7,14 @@ typedef void type_game_update(struct game_memory*, struct game_input*);
 type_game_init* game_init;
 type_game_update* game_update;
 
+// Todo: linux logging
+// - linux_log
+
 b32 linux_game_lib_load()
 {
     static void* game_lib = 0;
+
+    // Todo: check if library modified since load
 
     if (game_lib)
     {
@@ -32,6 +37,27 @@ b32 linux_game_lib_load()
 
     return false;
 }
+
+// Todo: linux high performance timer get
+// - linux_current_time_get
+// - linux_elapsed_time_get
+
+// Todo: linux file io
+// - linux_file_open
+// - linux_file_close
+// - linux_file_read
+// - linux_file_size_get
+
+// Todo: linux recording
+// - linux_recorded_memory_read
+// - linux_recorded_memory_write
+// - struct linux_recorded_inputs
+// - linux_recorded_inputs_read
+// - linux_recorded_inputs_write
+
+// Todo: get rid of globals
+struct file_functions file;
+struct opengl_functions gl;
 
 s32 main(s32 argc, char *argv[])
 {
@@ -58,6 +84,19 @@ s32 main(s32 argc, char *argv[])
     XClearWindow(display, window);
     XMapRaised(display, window);
 
+    // Todo: copy opengl function addresses
+    // Todo: copy file io addresses
+    
+    // Todo: fill struct game_init
+    // struct game_init init = { 0 };
+    // init.file = 0;
+    // init.gl = 0;
+    // init.log = 0;
+    // init.screen_width = 0;
+    // init.screen_height = 0;
+
+    // Todo: allocate game memory
+
     if (linux_game_lib_load())
     {
         printf("GAME LIB LOADED\n");
@@ -79,12 +118,17 @@ s32 main(s32 argc, char *argv[])
     
     while (true)
     {
+        // Todo: fill struct game_input
+        // Todo: calculate delta time
+        // Todo: read keyboard events
+        // Todo: read mouse events
+        // Todo: read other events?
+
+        // Todo: recording
+
+        // Todo: game_update
         XNextEvent(display, &ev);
     }
-
-    XDestroyWindow(display, window);
-    XFree(screen);
-    XCloseDisplay(display);
 
     return 0;
 }
