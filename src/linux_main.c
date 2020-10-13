04eafaed6d14b437f575edbc0ea5050d111450f1
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2020-10-13 13:53:43 +0300

    Linux: add empty platform functions

--- src/linux_main.c
+++ src/linux_main.c
@@ -1,4 +1,5 @@
 #include <X11/Xlib.h>
+#include <sys/mman.h>
 #include <dlfcn.h>
 #include "tk_platform.h"
 
@@ -7,8 +8,19 @@ typedef void type_game_update(struct game_memory*, struct game_input*);
 type_game_init* game_init;
 type_game_update* game_update;
 
-// Todo: linux logging
-// - linux_log
+void linux_log(char* format, ...)
+{
+    // Todo: implement own printf functions
+    char buffer[4096] = { 0 };
+    va_list args;
+
+    va_start(args, format);
+    vsnprintf(buffer, 4096, format, args);
+    va_end(args);
+
+    fprintf(stdout, "%s", buffer);
+    fflush(stdout);
+}
 
 b32 linux_game_lib_load()
 {
@@ -47,6 +59,22 @@ b32 linux_game_lib_load()
 // - linux_file_close
 // - linux_file_read
 // - linux_file_size_get
+void linux_file_open(file_handle* file, char* path, b32 read)
+{
+}
+
+void linux_file_close(file_handle* file)
+{
+}
+
+void linux_file_read(file_handle* file, s8* data, u64 bytes_max,
+    u64* bytes_read)
+{
+}
+
+void linux_file_size_get(file_handle* file, u64* file_size)
+{
+}
 
 // Todo: linux recording
 // - linux_recorded_memory_read
@@ -86,16 +114,27 @@ s32 main(s32 argc, char *argv[])
 
     // Todo: copy opengl function addresses
     // Todo: copy file io addresses
+
+    file.file_open = linux_file_open;
+    file.file_close = linux_file_close;
+    file.file_read = linux_file_read;
+    file.file_size_get = linux_file_size_get;
     
     // Todo: fill struct game_init
-    // struct game_init init = { 0 };
-    // init.file = 0;
-    // init.gl = 0;
-    // init.log = 0;
-    // init.screen_width = 0;
-    // init.screen_height = 0;
+    struct game_init init = { 0 };
+    init.file = &file;
+    init.gl = &gl;
+    init.log = linux_log;
+    init.screen_width = 0;
+    init.screen_height = 0;
 
     // Todo: allocate game memory
+    struct game_memory memory = { 0 };
+    memory.size = 1024*1024*1024;
+    memory.base = mmap(0, memory.size, PROT_READ|PROT_WRITE, 
+        MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+
+    game_init(&memory, &init);
 
     if (linux_game_lib_load())
     {
@@ -115,9 +154,13 @@ s32 main(s32 argc, char *argv[])
     {
         printf("Game update found yeees\n");
     }
+
+    // struct game_input old_input = { 0 };
     
     while (true)
     {
+        struct game_input new_input = { 0 };
+
         // Todo: fill struct game_input
         // Todo: calculate delta time
         // Todo: read keyboard events
@@ -128,6 +171,8 @@ s32 main(s32 argc, char *argv[])
 
         // Todo: game_update
         XNextEvent(display, &ev);
+
+        game_update(&memory, &new_input);
     }
 
     return 0;
