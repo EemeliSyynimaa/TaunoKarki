483fff006fb0d08691536a599109c56263fe2759
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2022-08-19 13:16:49 +0300

    Linux: implement high resolution timer

--- src/tk_time_api.h
+++ src/tk_time_api.h
@@ -1,8 +1,6 @@
-typedef u64 type_ticks_current_get();
-typedef u64 type_ticks_frequency_get();
+typedef u64 type_ticks_get();
 
 struct time_functions
 {
-    type_ticks_current_get* ticks_current_get;
-    type_ticks_frequency_get* ticks_frequency_get;
+    type_ticks_get* ticks_get;
 };
