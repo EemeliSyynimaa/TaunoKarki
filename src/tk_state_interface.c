d1c5de560adaa3e86bf1d0a57ac85ae4fc7c1e2b
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2023-03-14 20:10:43 +0200

    Create data struct for physics state

--- src/tk_state_interface.c
+++ src/tk_state_interface.c
@@ -1,10 +1,11 @@
-typedef void type_state_init(struct game_state*);
-typedef void type_state_update(struct game_state*, struct game_input*, f32);
-typedef void type_state_render(struct game_state*);
+typedef void type_state_init(void*);
+typedef void type_state_update(void*, struct game_input*, f32);
+typedef void type_state_render(void*);
 
 struct state_interface
 {
     type_state_init* init;
     type_state_update* update;
     type_state_render* render;
+    void* data;
 };
--- src/tk_state_physics.c
+++ src/tk_state_physics.c
@@ -1,18 +1,28 @@
-void state_physics_init(struct game_state* state)
+struct state_physics_data
 {
-    state->num_circles = 16;
+    struct game_state* base;
+    struct circle circles[MAX_CIRCLES];
+    struct contact contacts[MAX_CONTACTS];
+    struct line_segment lines[MAX_COLLISION_SEGMENTS];
+    u32 num_circles;
+    u32 num_lines;
+};
+
+void state_physics_init(struct state_physics_data* data)
+{
+    data->num_circles = 16;
 
     f32 area_size = 5.0f;
 
     // Make the first circle controllable
-    state->circles[0].position.x = 0.0f;
-    state->circles[0].position.y = 0.0f;
-    state->circles[0].radius = 0.25f;
-    state->circles[0].mass = 10.0f;
+    data->circles[0].position.x = 0.0f;
+    data->circles[0].position.y = 0.0f;
+    data->circles[0].radius = 0.25f;
+    data->circles[0].mass = 10.0f;
 
-    for (u32 i = 1; i < state->num_circles; i++)
+    for (u32 i = 1; i < data->num_circles; i++)
     {
-        struct circle* circle = &state->circles[i];
+        struct circle* circle = &data->circles[i];
         circle->position.x = f32_random(-area_size, area_size);
         circle->position.y = f32_random(-area_size, area_size);
         circle->radius = 0.25f;
@@ -22,31 +32,30 @@ void state_physics_init(struct game_state* state)
     }
 
     f32 size = 7.5f;
-    state->camera.screen_width = (f32)state->screen_width;
-    state->camera.screen_height = (f32)state->screen_height;
-    state->camera.position = (struct v3){ 0.0f, 0.0f, 5.0f };
-    state->camera.projection = m4_orthographic(-size, size, -size, size,
+    data->base->camera.position = (struct v3){ 0.0f, 0.0f, 5.0f };
+    data->base->camera.projection = m4_orthographic(-size, size, -size, size,
             0.1f, 100.0f);
-    state->camera.projection_inverse = m4_inverse(state->camera.projection);
+    data->base->camera.projection_inverse =
+        m4_inverse(data->base->camera.projection);
 
     struct v3 up = { 0.0f, 1.0f, 0.0f };
 
-    state->camera.view = m4_look_at(state->camera.position,
-        (struct v3) { state->camera.position.xy, 0.0f }, up);
-    state->camera.view_inverse = m4_inverse(state->camera.view);
+    data->base->camera.view = m4_look_at(data->base->camera.position,
+        (struct v3) { data->base->camera.position.xy, 0.0f }, up);
+    data->base->camera.view_inverse = m4_inverse(data->base->camera.view);
 
     api.gl.glClearColor(0.25f, 0.0f, 0.0f, 0.0f);
 }
 
-void state_physics_update(struct game_state* state, struct game_input* input,
-    f32 step)
+void state_physics_update(struct state_physics_data* data,
+    struct game_input* input, f32 step)
 {
     static f32 particle_test = 0;
     static u32 frame_number = 0;
 
     LOG("Frame: %i\n", frame_number++);
 
-    circles_velocities_update(state, input, step);
+    circles_velocities_update(data->circles, data->num_circles, input, step);
 
     f32 max_iterations = 10;
 
@@ -63,30 +72,42 @@ void state_physics_update(struct game_state* state, struct game_input* input,
         // Todo: sometimes collisions are handled poorly
         // when multiple circles are touching...?
 
-        state->num_contacts = 0;
+        u32 num_contacts = circles_collisions_check(data->circles,
+            data->num_circles, data->contacts, data->lines, data->num_lines);
 
-        circles_collisions_check(state);
-        circles_collisions_resolve(state, step);
-
-        if (!state->num_contacts)
+        if (num_contacts)
+        {
+            circles_collisions_resolve(data->contacts, num_contacts, step);
+        }
+        else
         {
             LOG("No contacts!\n");
             break;
         }
     }
 
-    circles_positions_update(state);
+    circles_positions_update(data->circles, data->num_circles);
 }
 
-void state_physics_render(struct game_state* state)
+void state_physics_render(struct state_physics_data* data)
 {
-    circles_render(state);
-    collision_map_render(state);
+    circles_render(data->circles, data->num_circles, data->base);
+    // collision_map_render(state);
 }
 
-struct state_interface state_physics =
+struct state_interface state_physics_create(struct game_state* state)
 {
-    .init = state_physics_init,
-    .update = state_physics_update,
-    .render = state_physics_render
-};
+    struct state_interface result = { 0 };
+    result.init = state_physics_init;
+    result.update = state_physics_update;
+    result.render = state_physics_render;
+    result.data = stack_alloc(&state->stack, sizeof(struct state_physics_data));
+
+    memory_set(result.data, sizeof(struct state_physics_data), 0);
+
+    struct game_state** base = (struct game_state**)result.data;
+
+    *base = state;
+
+    return result;
+}
