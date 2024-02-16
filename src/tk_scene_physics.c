383c0158e1396b736b14031ea4581bab10f81e17
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2024-02-17 00:36:05 +0200

    Render AABB

--- src/tk_scene_physics.c
+++ src/tk_scene_physics.c
@@ -85,6 +85,70 @@ void render_circle(f32 x, f32 y, f32 radius)
     api.gl.glBindVertexArray(0);
 }
 
+void render_aabb(f32 x, f32 y, f32 half_width, f32 half_height)
+{
+    // Todo: just experimental code... :-)
+    u32 vao;
+    u32 vbo;
+    u32 ibo;
+    u32 texture = 0;
+
+    struct v4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+    struct vertex vertices[] =
+    {
+        { { x - half_width, y - half_height, 0 }, v2_zero, v3_zero, color },
+        { { x + half_width, y + half_height, 0 }, v2_zero, v3_zero, color },
+        { { x - half_width, y + half_height, 0 }, v2_zero, v3_zero, color },
+        { { x + half_width, y - half_height, 0 }, v2_zero, v3_zero, color },
+    };
+
+    u32 indices[6] = { 0, 1, 2, 0, 3, 1 };
+
+    api.gl.glGenVertexArrays(1, &vao);
+    api.gl.glBindVertexArray(vao);
+
+    api.gl.glGenBuffers(1, &vbo);
+    api.gl.glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    api.gl.glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+        GL_DYNAMIC_DRAW);
+
+    api.gl.glGenBuffers(1, &ibo);
+    api.gl.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    api.gl.glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+        GL_DYNAMIC_DRAW);
+
+    api.gl.glEnableVertexAttribArray(0);
+    api.gl.glEnableVertexAttribArray(1);
+    api.gl.glEnableVertexAttribArray(2);
+    api.gl.glEnableVertexAttribArray(3);
+
+    // Todo: implement offsetof
+    api.gl.glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+        sizeof(struct vertex), (void*)0);
+    api.gl.glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+        sizeof(struct vertex), (void*)12);
+    api.gl.glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,
+        sizeof(struct vertex), (void*)20);
+    api.gl.glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE,
+        sizeof(struct vertex), (void*)32);
+
+    api.gl.glUseProgram(shader);
+
+    u32 uniform_mvp = api.gl.glGetUniformLocation(shader, "MVP");
+    u32 uniform_color = api.gl.glGetUniformLocation(shader,
+        "uniform_color");
+
+    api.gl.glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE,
+        (GLfloat*)&view_projection);
+    api.gl.glUniform4fv(uniform_color, 1, (GLfloat*)&color);
+
+    api.gl.glDrawElements(GL_TRIANGLES, ARRAY_SIZE(indices), GL_UNSIGNED_INT,
+        NULL);
+
+    api.gl.glUseProgram(0);
+    api.gl.glBindVertexArray(0);
+}
+
 void scene_physics_init(struct scene_physics* data)
 {
     // Todo: maybe this function should return the initialized struct instead
@@ -103,5 +167,6 @@ void scene_physics_update(struct scene_physics* data, struct game_input* input,
 
 void scene_physics_render(struct scene_physics* data)
 {
-    render_circle(data->circle_x, data->circle_y, 200.0f);
+    render_circle(200.0f, 200.0f, 50.0f);
+    render_aabb(400.0f, 200.0f, 100.0f, 50.0f);
 }
