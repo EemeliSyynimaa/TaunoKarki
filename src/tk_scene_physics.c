37b2026d6ed1f93d4da5f32c97152d34790632f9
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2024-02-18 14:34:53 +0200

    Create basic entity struct

--- src/tk_scene_physics.c
+++ src/tk_scene_physics.c
@@ -11,12 +11,56 @@ struct scene_physics scene_physics_init(struct game_state* game)
 void scene_physics_update(struct scene_physics* data, struct game_input* input,
     f32 step)
 {
+    // Todo: temporary here to reset everything on hot reload
+    memory_set(&data->entities, sizeof(data->entities), 0);
+    data->num_entities = 0;
+
+    struct tk_entity* circle = &data->entities[data->num_entities++];
+    circle->x = 200.0f;
+    circle->y = 200.0f;
+    circle->radius = 50.0f;
+    circle->color = colors[AQUA];
+    circle->flags = TK_ENTITY_ALIVE | TK_ENTITY_RENDERABLE;
+    circle->render_type = TK_RENDER_TYPE_CIRCLE;
+    circle->num_fans = 32;
+
+    struct tk_entity* aabb = &data->entities[data->num_entities++];
+    aabb->x = 400.0f;
+    aabb->y = 200.0f;
+    aabb->half_width = 100.0f;
+    aabb->half_height = 50.0f;
+    aabb->color = colors[OLIVE];
+    aabb->flags = TK_ENTITY_ALIVE | TK_ENTITY_RENDERABLE;
+    aabb->render_type = TK_RENDER_TYPE_AABB;
 }
 
 void scene_physics_render(struct scene_physics* data)
 {
-    renderer_draw_circle(&data->renderer, 200.0f, 200.0f, 50.0f, colors[TEAL],
-        32);
-    renderer_draw_aabb(&data->renderer, 400.0f, 200.0f, 100.0f, 50.0f,
-        colors[OLIVE]);
+    for (u32 i = 0; i < data->num_entities; i++)
+    {
+        struct tk_entity* entity = &data->entities[i];
+
+        if (entity->flags & TK_ENTITY_ALIVE)
+        {
+            if (entity->flags & TK_ENTITY_RENDERABLE)
+            {
+                switch (entity->render_type)
+                {
+                    case TK_RENDER_TYPE_CIRCLE:
+                    {
+                        renderer_draw_circle(&data->renderer, entity->x,
+                            entity->y, entity->radius, entity->color,
+                            entity->num_fans);
+
+                    } break;
+                    case TK_RENDER_TYPE_AABB:
+                    {
+                        renderer_draw_aabb(&data->renderer, entity->x,
+                            entity->y, entity->half_width, entity->half_height,
+                            entity->color);
+                    } break;
+                }
+            }
+        }
+    }
 }
--- src/tk_scene_physics.h
+++ src/tk_scene_physics.h
@@ -1,4 +1,44 @@
+// Todo: these structs are temporarily named to avoid conflicts
+
+enum
+{
+    TK_ENTITY_ALIVE = 1 << 0,
+    TK_ENTITY_RENDERABLE = 1 << 1
+};
+
+enum
+{
+    TK_RENDER_TYPE_CIRCLE,
+    TK_RENDER_TYPE_AABB
+};
+
+struct tk_entity
+{
+    u64 flags;
+
+    f32 x;
+    f32 y;
+
+    // Render types
+    u32 render_type;
+
+    struct v4 color;
+
+    // Circle
+    f32 radius;
+    u32 num_fans;
+
+    // AABB
+    f32 half_width;
+    f32 half_height;
+};
+
+#define TK_MAX_ENTITIES 256
+
 struct scene_physics
 {
     struct renderer renderer;
+
+    struct tk_entity entities[TK_MAX_ENTITIES];
+    u32 num_entities;
 };
